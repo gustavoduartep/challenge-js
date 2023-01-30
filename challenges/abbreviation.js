3881922995b19b0a6ed0b15c5a1ce5554426dc4a
Gustavo Duarte <gustavoduartepedrosa@gmail.com>
2023-01-30 20:19:45 -0300

    Primeiro algoritmo

---
+++ challenges/abbreviation.js
@@ -0,0 +1,19 @@
+function abbreviation(a, b) {
+    a = "AbcDE";
+    b = "ABDE";
+    let aCaptalize = a.toUpperCase();
+    let n = aCaptalize.length;
+    let m = b.length;
+    let dinamico = [];
+    
+    for(let i = 0; i < n; i++) {
+        for(let s = 0; s <= m; s++) {
+            if(s < m && a[i].toUpperCase() == b[s] ) {
+                dinamico.push(aCaptalize[i])
+                var resultado = dinamico.join("")         
+        }
+    }
+    }
+    return console.log(resultado === b ? "Yes" : "No");
+}
+abbreviation("AbcDE", "ABDE");
